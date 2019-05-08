dbd7adc0e4b74effd37ef22ef9fa81e7c06bcbbc
Kean <wei16008@byui.edu>
2019-05-07 22:53:42 -0400

    added 03teamactivity html and php

---
+++ web/03teamactivity.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Name: <?php echo $_POST["name"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Major: <?php echo $_POST["major"]; ?><br>
+Comments: <?php echo $_POST["comments"]; ?><br>
+Continents: <?php echo $_POST["continents"]; ?><br>
+
+</body>
+</html>
