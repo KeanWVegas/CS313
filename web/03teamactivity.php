2ba8c0ab0bf1d7201ee33bedcb8b4281802322ba
Kean <wei16008@byui.edu>
2019-05-07 23:15:24 -0400

    php fixes 03teamactivity

--- web/03teamactivity.php
+++ web/03teamactivity.php
@@ -5,13 +5,13 @@ Name: <?php echo $_POST["name"]; ?><br>
 Email: <?php echo $_POST["email"]; ?><br>
 Major: <?php echo $_POST["major"]; ?><br>
 Comments: <?php echo $_POST["comments"]; ?><br>
-Continents: <?php echo $_POST["continentsna"]; ?><br>
-Continents: <?php echo $_POST["continentssa"]; ?><br>    
-Continents: <?php echo $_POST["continentseu"]; ?><br>
-Continents: <?php echo $_POST["continentsas"]; ?><br>
-Continents: <?php echo $_POST["continentsaf"]; ?><br>
-Continents: <?php echo $_POST["continentsau"]; ?><br>
-Continents: <?php echo $_POST["continentsan"]; ?><br>
+ <?php echo $_POST["continentsna"]; ?><br>
+ <?php echo $_POST["continentssa"]; ?><br>    
+ <?php echo $_POST["continentseu"]; ?><br>
+ <?php echo $_POST["continentsas"]; ?><br>
+ <?php echo $_POST["continentsaf"]; ?><br>
+ <?php echo $_POST["continentsau"]; ?><br>
+ <?php echo $_POST["continentsan"]; ?><br>
 
 </body>
 </html>
