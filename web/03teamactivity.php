7dd9865689b7d30ccdc15fb76b44c8fa6a93a934
Kean <wei16008@byui.edu>
2019-05-07 22:58:17 -0400

    php fixes

--- web/03teamactivity.php
+++ web/03teamactivity.php
@@ -6,6 +6,12 @@ Email: <?php echo $_POST["email"]; ?><br>
 Major: <?php echo $_POST["major"]; ?><br>
 Comments: <?php echo $_POST["comments"]; ?><br>
 Continents: <?php echo $_POST["continents"]; ?><br>
+Continents: <?php echo $_POST["continents"]; ?><br>    
+Continents: <?php echo $_POST["continents"]; ?><br>
+Continents: <?php echo $_POST["continents"]; ?><br>
+Continents: <?php echo $_POST["continents"]; ?><br>
+Continents: <?php echo $_POST["continents"]; ?><br>
+Continents: <?php echo $_POST["continents"]; ?><br>
 
 </body>
 </html>
