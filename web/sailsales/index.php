d7bfa075e39e4f4aa72b7ff960a9d07a59817c0c
Kean <wei16008@byui.edu>
2019-05-08 23:32:45 -0400

    created externall php file

--- web/sailsales/index.php
+++ web/sailsales/index.php
@@ -11,7 +11,7 @@
 </header>
 
 <body>
-    <form action="" method="post">
+    <form action="/phptest.php" method="post">
         <input type="" value="Dreamyboat" name="item"> <br>
         <img src="../images/headshot.jpg" id="itemimg" name="productimg"> <br>
         <input type="" value="$3.50" name="price"><br>
@@ -20,15 +20,6 @@
         <input type="submit" value="Add to Cart">
     </form>
 
-    <?php
-    $form_array = array("item", "productimg", "price", "quantity")
-    $cookie_name ="cart";
-    $cookie_value = "quantity"
-    setcookie($cookie_name, $cookie_value, $form_array, time() + (86400 * 30), "/");
-    
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);s
-?>
 
 </body>
 
