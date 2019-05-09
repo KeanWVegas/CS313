efb22c3619a80c1f5a4e9d7d6090183c7b35bab6
Kean <wei16008@byui.edu>
2019-05-08 23:42:01 -0400

    forgot a ;

--- web/sailsales/index.php
+++ web/sailsales/index.php
@@ -22,7 +22,7 @@
     
 <?php
     $cookie_name ="cart";
-    $cookie_value = $_POST['test']
+    $cookie_value = $_POST['test'];
     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
     
 ?>
