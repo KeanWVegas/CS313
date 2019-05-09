107b6e94ad9da917a024aa7df859a8474a54943d
Kean <wei16008@byui.edu>
2019-05-08 23:38:57 -0400

    testing

--- web/sailsales/index.php
+++ web/sailsales/index.php
@@ -12,11 +12,11 @@
 
 <body>
     <form action="phptest.php" method="post">
-        <input type="" value="Dreamyboat" name="item"> <br>
-        <img src="../images/headshot.jpg" id="itemimg" name="productimg"> <br>
-        <input type="" value="$3.50" name="price"><br>
+        <input type=""                    name="test[]"value="Dreamyboat" > <br>
+        <img src="../images/headshot.jpg" name="test[]" id="itemimg" > <br>
+        <input type=""                    name="test[]" value="$3.50" ><br>
         Quantity:
-        <input type="number" name="quantity" min="1" max="99" maxlength="2" value="1"><br>
+        <input type="number"              name="test[]" min="1" max="99" maxlength="2" value="1"><br>
         <input type="submit" value="Add to Cart">
     </form>
 
--- web/sailsales/phptest.php
+++ web/sailsales/phptest.php
@@ -1,9 +1,6 @@
 <?php
-    $form_array = array("item", "productimg", "price", "quantity")
     $cookie_name ="cart";
-    $cookie_value = "quantity"
+    $cookie_value = $_POST['test']
     setcookie($cookie_name, $cookie_value, $form_array, time() + (86400 * 30), "/");
     
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);s
 ?>
