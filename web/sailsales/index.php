e8e1c97d1067e5f97333a15754527249a3f31ec7
Kean <wei16008@byui.edu>
2019-05-08 23:31:25 -0400

    debug php addition

--- web/sailsales/index.php
+++ web/sailsales/index.php
@@ -25,6 +25,9 @@
     $cookie_name ="cart";
     $cookie_value = "quantity"
     setcookie($cookie_name, $cookie_value, $form_array, time() + (86400 * 30), "/");
+    
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);s
 ?>
 
 </body>
