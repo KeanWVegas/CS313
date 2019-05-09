bc9220fe921040f04634328bad158a0a9a0d5f68
Kean <wei16008@byui.edu>
2019-05-08 21:53:01 -0400

    first sailsales commit, added home and cart pages

---
+++ web/sailsales/cart.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sail Sales Cart</title>
+</head>
+<header>
+    <h1>Sails for Sale</h1>
+    <h2>Sell your sails at sale prices!</h2>
+
+</header>
+
+<body>
+<?php echo $_POST["sailboat01"]; ?>
+
+</body></html>
