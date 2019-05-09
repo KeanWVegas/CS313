48a84d0fb46705649207aab9c9789f5b57f8dba0
Kean <wei16008@byui.edu>
2019-05-08 23:40:37 -0400

    broke stuff again??

--- web/sailsales/index.php
+++ web/sailsales/index.php
@@ -11,7 +11,7 @@
 </header>
 
 <body>
-    <form action="phptest.php" method="post">
+    <form action="" method="post">
         <input type=""                    name="test[]"value="Dreamyboat" > <br>
         <img src="../images/headshot.jpg" name="test[]" id="itemimg" > <br>
         <input type=""                    name="test[]" value="$3.50" ><br>
@@ -19,7 +19,13 @@
         <input type="number"              name="test[]" min="1" max="99" maxlength="2" value="1"><br>
         <input type="submit" value="Add to Cart">
     </form>
-
+    
+<?php
+    $cookie_name ="cart";
+    $cookie_value = $_POST['test']
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+    
+?>
 
 </body>
 
