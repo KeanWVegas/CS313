d36efb39c397a0face60fde37e475aa39dd927e4
Kean <wei16008@byui.edu>
2019-05-11 23:44:10 -0700

    testing confirmation page printing

--- web/sailsales/checkout.php
+++ web/sailsales/checkout.php
@@ -1,6 +1,11 @@
 <?php
 
 session_start();
+
+$_SESSION['address'] = $address = array (
+    $_POST['street'], $_POST['city'], $_POST['state'], $_POST['zip']
+    );
+
 if (isset($_POST['submit'])) { 
     $_SESSION['address'][] = $_POST['address'];
 }
@@ -18,18 +23,19 @@ if (isset($_POST['submit'])) {
     <h2> Please enter your shipping address</h2>
     <form action='confirmation.php' method='post'>
         Street Address:
-        <input type="text" name="address" value="address" onfocus=""><br>
+        <input type="text" name="street" value="address" onfocus="this.value=''"><br>
         City:
-        <input type="text" name="address" value="address" onfocus=""><br>
+        <input type="text" name="city" value="address" onfocus="this.value=''"><br>
         State:
-        <input type="text" name="address" value="address" onfocus=""><br>
+        <input type="text" name="state" value="address" onfocus="this.value=''"><br>
         Zip Code:
-        <input type="text" name="address" value="address" onfocus=""><br>    
-        
+        <input type="text" name="zip" value="address" onfocus="this.value=''"><br>    
         <input type="button" onclick="window.location.href = 'index.php';" value="Return Home"><br>
     
         <button type="submit" name='submit' >Complete Checkout</button>
         
     </form>
-    
+    <?php 
+        echo(array_values($SESSION['address']);
+             ?>
 </body>
