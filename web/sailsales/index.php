bf5f7ae5beb2fbd54acc1d2d41d2cfd64ae655f1
Kean <wei16008@byui.edu>
2019-05-11 22:20:36 -0400

    added cart button

--- web/sailsales/index.php
+++ web/sailsales/index.php
@@ -59,4 +59,29 @@ else if (isset ( $_POST ['delete'] )) { // a remove button has been clicked
 
     </form>
 
+    
+    <h1>Cart:</h1>
+<?php
+// Set a default total
+$total = 0;
+foreach ( $_SESSION['cart'] as $ino ) {
+    ?>
+<tr>
+    <td>
+        Name: <?php echo $items[$ino]['name']; ?>
+    </td>
+    <td>
+        Price: <?php echo $items[$ino]["price"]; ?>
+    </td>
+    <td>
+        <button type='submit' name='delete' value='<?php echo $ino; ?>'>Remove</button>
+    </td>
+</tr>
+<?php
+    $total += $items[$ino]['price'];
+} // end foreach
+?>
+
+Total: $<?php echo $total; ?>
+
 </body>
