a623b56e11d43bd37b65f86f18adf6d177df8790
Kean <wei16008@byui.edu>
2019-05-11 22:55:35 -0400

    added cart button

--- web/sailsales/index.php
+++ web/sailsales/index.php
@@ -68,10 +68,10 @@ foreach ( $_SESSION['cart'] as $ino ) {
     ?>
 <tr>
     <td>
-        Name: <?php echo $items[$ino]['name']; ?>
+        Name: <?php echo $items[$ino]['name']; ?><br>
     </td>
     <td>
-        Price: <?php echo $items[$ino]["price"]; ?>
+        Price: <?php echo $items[$ino]["price"]; ?><br>
     </td>
     <td>
         <button type='submit' name='remove' value='<?php echo $ino; ?>'>Remove</button>
