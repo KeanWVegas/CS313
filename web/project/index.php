ab2f3c44c3dbae1208ced5ccde2fe133a4df96b9
Kean <wei16008@byui.edu>
2019-05-25 11:23:24 -0700

    adding project

--- web/project/index.php
+++ web/project/index.php
@@ -25,8 +25,8 @@
         echo "<table>";
         while($row = pg_fetch_assoc($result)) {
             echo "<tr>";
-            echo "<td align='center' width='200'>" . $row[0] . "</td>";
-            echo "<td align='center' width='200'>" . $row[1] . "</td>";
+            echo "<td align='center' width='200'>" . $row['id'] . "</td>";
+            echo "<td align='center' width='200'>" . $row['name'] . "</td>";
             echo "</tr>";
             echo "</table>";
             
