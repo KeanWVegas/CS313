c80e4fda6aba6ba43b66b48be9c1a8f42cec5bd2
Kean <wei16008@byui.edu>
2019-05-25 11:12:50 -0700

    adding project

--- web/project/index.php
+++ web/project/index.php
@@ -18,10 +18,12 @@
             echo "<tr>";
             echo "<td align='center' width='200'>" . $row['id'] . "</td>";
             echo "<td align='center' width='200'>" . $row['name'] . "</td>";
-            echo "</tr>";echo "</table>";
+            echo "</tr>";
+            echo "</table>";
             
-                
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
     ?>
 </body>
-
 </html>
