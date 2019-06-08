9af5ed4977559d1e01bf8679707253624514f3f3
Kean <wei16008@byui.edu>
2019-06-08 16:38:07 -0700

    fix page?

--- web/project/index.php
+++ web/project/index.php
@@ -16,9 +16,7 @@
             exit;
         }     
     
-        $resultusers = pg_query($db, "SELECT * FROM public.users");
-        $resultlists = pg_query($db, "SELECT * FROM public.lists");
-    
+        $result = pg_query($db, "SELECT * FROM public.users");    
         if (!result) {
             echo "An error occured in querying the database\n";
             exit;
@@ -30,7 +28,7 @@
                 echo "<td align='center' width='200'>Name</td>";
             echo "</tr>";
     
-        while($row = pg_fetch_assoc($resultusers)) {
+        while($row = pg_fetch_assoc($result)) {
                 echo "<tr>";
                     echo "<td align='center' width='200'>" . $row['id'] . "</td>";
                     echo "<td align='center' width='200'>" . $row['name'] . "</td>";
