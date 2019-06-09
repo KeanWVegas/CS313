b57ad2e597a40b5c2ccca4b5b26d4691de72cc53
Kean <wei16008@byui.edu>
2019-06-08 20:29:07 -0700

    let's pray this works

--- web/project/index.php
+++ web/project/index.php
@@ -1,3 +1,11 @@
+<?php
+     $db = pg_pconnect("host=ec2-54-163-230-199.compute-1.amazonaws.com port=5432 dbname=d7bihr2q4jfbob user=dkqfstgvudohch password=8f311282575d69028cc57c8d3ec16dcb5f1c3af3ef91bded21fd0f7bc94f2061");
+        if (!$db) {
+            echo "An error occured in connecting to the database\n";
+            exit;
+        }     
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -8,15 +16,19 @@
 <body>
     <h1>Grocery Sorcery</h1>
     <h3>The eezypeezylemonsqueezy way to track your groceries</h3>
-
-    <?php
-        $db = pg_pconnect("host=ec2-54-163-230-199.compute-1.amazonaws.com port=5432 dbname=d7bihr2q4jfbob user=dkqfstgvudohch password=8f311282575d69028cc57c8d3ec16dcb5f1c3af3ef91bded21fd0f7bc94f2061");
-        if (!$db) {
-            echo "An error occured in connecting to the database\n";
-            exit;
-        }     
     
-        $resultusers = pg_query($db, "SELECT * FROM public.lists");
+    <form name="insert" method="post" action="/insertForm.php">
+        <select name='status'>
+            <option value='need' selected>Need</option>
+            <option value='cart'>Cart</option>
+            <option value='have'>Have</option>
+        </select>
+        <input type="text" value="Item Name">
+        <input type="submit" value='Submit'>
+    </form>
+    <?php
+        
+        $result = pg_query($db, "SELECT * FROM public.lists");
     
         if (!result) {
             echo "An error occured in querying the database\n";
@@ -29,7 +41,7 @@
                 echo "<td align='center' width='200'>Item</td>";
             echo "</tr>";
     
-        while($row = pg_fetch_assoc($resultusers)) {
+        while($row = pg_fetch_assoc($result)) {
                 echo "<tr>";
                     echo "<td align='center' width='200'>" . $row['status'] . "</td>";
                     echo "<td align='center' width='200'>" . $row['item'] . "</td>";
@@ -42,5 +54,6 @@
             ini_set('display_startup_errors', 1);
             error_reporting(E_ALL);
     ?>
+          
 </body>
 </html>
---
+++ web/project/insertForm.php
@@ -0,0 +1,10 @@
+<?php    
+$db = pg_pconnect("host=ec2-54-163-230-199.compute-1.amazonaws.com port=5432 dbname=d7bihr2q4jfbob user=dkqfstgvudohch password=8f311282575d69028cc57c8d3ec16dcb5f1c3af3ef91bded21fd0f7bc94f2061");
+        if (!$db) {
+            echo "An error occured in connecting to the database\n";
+            exit;
+        }  
+            $insertQuery = 'INSERT INTO lists (status, item, userid) VALUES ('$_POST[status]', '$_POST[item]', '$_POST[userid]')';
+            $result = pg_query($query);
+
+    ?>
