610f23b95908846592b3566739da77a0dfa99d3d
Kean <wei16008@byui.edu>
2019-06-08 20:29:50 -0700

    let's pray this works

--- web/project/index.php
+++ web/project/index.php
@@ -17,7 +17,7 @@
     <h1>Grocery Sorcery</h1>
     <h3>The eezypeezylemonsqueezy way to track your groceries</h3>
     
-    <form name="insert" method="post" action="/insertForm.php">
+    <form name="insert" method="post" action="insertForm.php">
         <select name='status'>
             <option value='need' selected>Need</option>
             <option value='cart'>Cart</option>
