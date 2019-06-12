faf962104af0039da1db682c80691e4202cc918c
Kean <wei16008@byui.edu>
2019-06-11 22:22:30 -0700

    test

---
+++ web/loginsession.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <form name='login' method="POST" action="login.php">
+        <input name="name" type="text">
+        <input type='submit'>
+    </form>
+</html>
--- web/project/login.php
+++ web/project/login.php
@@ -5,23 +5,14 @@
 
     session_start();
     
-    
     require("dbConnect.php");
 
     $db = get_db();
 //select userid from user table where id=:id 
-    
-    
-
 ?>
-<!DOCTYPE html>
-<html>
-    <form name='login' method="POST" action="">
-        <input name="username" type="text">
-        <input type='submit'>
-    </form>
+
 <?php
-    $name = $_POST["username"];
+    $name = $_POST["name"];
     $stmt = $db->prepare('SELECT id FROM users WHERE name=:name');
 
     $stmt->bindValue(':name, $name');
@@ -30,4 +21,3 @@
 
     $_SESSION["userid"] = $rows[0]['id'];
 ?>
-</html>
