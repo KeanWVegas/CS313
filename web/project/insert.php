6f46edc7b4d50ce3206422195f9916683d7a8044
Kean <wei16008@byui.edu>
2019-06-11 21:39:25 -0700

    test

--- web/project/insert.php
+++ web/project/insert.php
@@ -3,7 +3,7 @@
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
 
-        $db1 = new pg_pconnect("host=ec2-54-163-230-199.compute-1.amazonaws.com port=5432 dbname=d7bihr2q4jfbob user=dkqfstgvudohch password=8f311282575d69028cc57c8d3ec16dcb5f1c3af3ef91bded21fd0f7bc94f2061");
+        $db1 = pg_pconnect("host=ec2-54-163-230-199.compute-1.amazonaws.com port=5432 dbname=d7bihr2q4jfbob user=dkqfstgvudohch password=8f311282575d69028cc57c8d3ec16dcb5f1c3af3ef91bded21fd0f7bc94f2061");
         if (!$db) {
             echo "An error occured in connecting to the database\n";
             exit;
