a410ec1db5da99ff45cd6d77dfd17db6f8f4b44d
Kean <wei16008@byui.edu>
2019-06-11 22:07:03 -0700

    test

---
+++ web/project/login.php
@@ -0,0 +1,22 @@
+<?PHP
+
+    session_start();
+$name = $_POST["username"];
+$db = get_db();
+//select userid from user table where id=:id 
+    
+    $stmt = $db->prepare('SELECT * FROM users WHERE name=:name');
+
+    $stmt->bindValue(':name, $name');
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $_SESSION["userid"] = $rows[0]['id'];
+?>
+<!DOCTYPE html>
+<html>
+    <form name='login' method="POST">
+        <input name="username" type="text">
+        <input type='submit'>
+    </form>
+</html>
