07c1a8805cffa3cf759c7c549018721d6b7ee387
Kean <wei16008@byui.edu>
2019-06-11 22:32:34 -0700

    test

--- web/project/insert.php
+++ web/project/insert.php
@@ -3,19 +3,22 @@
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);   
         
+        session_start();
+
         require ("dbConnect.php");
 
         $db = get_db();
 
         $status = htmlspecialchars($_POST['status']);
-        $item = htmlspecialchars($_POST['item']);        
+        $item = htmlspecialchars($_POST['item']); $id = $_SESSION['userid'];       
         
-        $insertquery = "INSERT INTO lists (status, item, userid) VALUES (:status, :item, 3)";
+        $insertquery = "INSERT INTO lists (status, item, userid) VALUES (:status, :item, :id)";
 try{
         $stmt = $db->prepare($insertquery);
 
         $stmt->bindValue(':status', $status);
         $stmt->bindValue(':item', $item);
+        $stmt->bindValue(':id', $id);
         $stmt->execute();}
         
         header("Location: index.php");
