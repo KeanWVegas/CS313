b4f64d49e3ed87d6412702421159ae77b59b83ad
Kean <wei16008@byui.edu>
2019-06-14 01:00:49 -0700

    test

--- web/project/index.php
+++ web/project/index.php
@@ -45,14 +45,11 @@
         if (!isset(id) {
             echo "Please sign in to continue:";
             echo "   <div class='container'>
-       <!-- Trigger the modal with a button -->
        <button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Sign In</button>
 
-       <!-- Modal -->
        <div class='modal fade' id='myModal' role='dialog'>
            <div class='modal-dialog'>
 
-               <!-- Modal content-->
                <div class='modal-content'>
                    <div class='modal-header'>
                        <button type='button' class='close' data-dismiss='modal'>&times;</button>
