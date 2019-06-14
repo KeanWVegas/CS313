e2a48974953ef229b582f8e9077b91064cff0f6c
Kean <wei16008@byui.edu>
2019-06-14 01:18:44 -0700

    test

--- web/project/index.php
+++ web/project/index.php
@@ -42,9 +42,32 @@
         
         $id = $_SESSION['userid'];
         
-        if ($id) {
+        if (!$id) {
             echo "Please sign in to continue";
+            echo "   <div class='container'>
+       <!-- Trigger the modal with a button -->
+       <button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Sign In</button>
 
+       <!-- Modal -->
+       <div class='modal fade' id='myModal' role='dialog'>
+           <div class='modal-dialog'>
+
+               <!-- Modal content-->
+               <div class='modal-content'>
+                   <div class='modal-header'>
+                       <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                       <h4 class='modal-title'>Modal Header</h4>
+                   </div>
+                   <div class='modal-body'>
+                       <p>Some text in the modal.</p>
+                   </div>
+                   <div class='modal-footer'>
+                       <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+                   </div>
+               </div>
+           </div>
+       </div>
+";
         }
         else {
             echo "Welcome, " . $id;
