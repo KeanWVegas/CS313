0d2131c494a91cfb9c7b094a1ffe992dbfbc1d40
Kean <wei16008@byui.edu>
2019-06-15 13:04:22 -0700

    closer

--- web/project/signup.php
+++ web/project/signup.php
@@ -11,7 +11,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
-
+<body style='max-width:960px; margin:auto;'>
 <h1>Grocery Sorcery</h1>
     <h3>The eezypeezy grocery list</h3>
 
@@ -20,3 +20,4 @@
      <input name='name' type='text'>
      <input class='btn btn-default' type='submit'>
 </form>
+</body>
