e65a5466475d65715fd909c00e0a8296227f05cc
Kean <wei16008@byui.edu>
2019-06-15 13:03:21 -0700

    closer

--- web/project/signup.php
+++ web/project/signup.php
@@ -1,11 +1,16 @@
 <?php
-           
-
+        
     require ("dbConnect.php");
     $db = get_db();   
 
     session_start();
 ?>
+<head>
+    <title> Grocery Sorcery Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
 
 <h1>Grocery Sorcery</h1>
     <h3>The eezypeezy grocery list</h3>
