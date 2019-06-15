22ee385b5f186f9c141f9807a08c494903d42364
Kean <wei16008@byui.edu>
2019-06-15 00:14:30 -0700

    test

--- web/project/index.php
+++ web/project/index.php
@@ -60,9 +60,36 @@
                        <h4 class='modal-title'>Modal Header</h4>
                    </div>
                    <div class='modal-body'>
+                   
                        <form name='login' method='POST' action='login.php'>
         <input name='name' type='text'>
         <input type='submit'>
+        <div class='create account'>
+       <!-- Trigger the modal with a button -->
+       <button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Sign In</button>
+
+       <!-- Modal -->
+       <div class='modal fade' id='myModal' role='dialog'>
+           <div class='modal-dialog'>
+
+               <!-- Modal content-->
+               <div class='modal-content'>
+                   <div class='modal-header'>
+                       <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                       <h4 class='modal-title'>Modal Header</h4>
+                   </div>
+                   <div class='modal-body'>
+                       <form name='login' method='POST' action='login.php'>
+        <input name='name' type='text'>
+        <input type='submit'>
+    </form>
+                   </div>
+                   <div class='modal-footer'>
+                       <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+                   </div>
+               </div>
+           </div>
+       </div>";
     </form>
                    </div>
                    <div class='modal-footer'>
