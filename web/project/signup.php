8a000b60aa072ca3dddaaf0206d7677b0ca49c23
Kean <wei16008@byui.edu>
2019-06-15 13:03:35 -0700

    closer

--- web/project/signup.php
+++ web/project/signup.php
@@ -16,7 +16,7 @@
     <h3>The eezypeezy grocery list</h3>
 
 <form name='login' method='POST' action='insertuser.php'>
-    <h5> Enter a username: </h5>
+    <h4> Enter a username: </h4>
      <input name='name' type='text'>
      <input class='btn btn-default' type='submit'>
 </form>
