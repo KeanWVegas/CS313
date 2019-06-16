4a6154e477221098ba2213564dea286c111d0c24
Kean <wei16008@byui.edu>
2019-06-16 04:08:54 -0700

    closer

--- web/project/insertuser.php
+++ web/project/insertuser.php
@@ -1,29 +1,21 @@
-<?PHP
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-    session_start();
+<?php
     
     require("dbConnect.php");
 
     $db = get_db();
-//select userid from user table where id=:id
-    $name = $_POST["name"];
+
+    $name = $_POST['name'];
     
-    $insertsql = 'INSERT INTO users (name) VALUES (':name');';
+    $insertsql = 'INSERT INTO users (name) VALUES ('testtest')';
     $stmt = $db->prepare($insertsql);
 
-    $stmt->bindValue(':name', $name);
+    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
     $stmt->execute();
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     $_SESSION["userid"] = $rows[0]['id'];
 
     header("Location: index.php");
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
     die();
     }
 ?>
--- web/project/signup.php
+++ web/project/signup.php
@@ -10,12 +10,13 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    
 </head>
 <body style='max-width:960px; margin:auto;'>
 <h1>Grocery Sorcery</h1>
     <h3>The eezypeezy grocery list</h3>
 
-<form name='login' method='POST' action='insertuser.php'>
+<form name='createuser' method='POST' action='insertuser.php'>
     <h4> Enter a username: </h4>
      <input class='btn btn-default' name='name' type='text'>
      <input class='btn btn-default' type='submit'>
