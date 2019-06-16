41e433c7a81086f8e8a13f5a394bb53bf29f8a6a
Kean <wei16008@byui.edu>
2019-06-16 03:50:44 -0700

    closer

--- web/project/insertuser.php
+++ web/project/insertuser.php
@@ -11,7 +11,7 @@ error_reporting(E_ALL);
 //select userid from user table where id=:id
     $name = $_POST["name"];
     
-    $insertsql = 'INSERT INTO users (name) VALUES (:name)';
+    $insertsql = 'INSERT INTO users (name) VALUES (':name')';
     $stmt = $db->prepare($insertsql);
 
     $stmt->bindValue(':name', $name);
@@ -21,6 +21,9 @@ error_reporting(E_ALL);
     $_SESSION["userid"] = $rows[0]['id'];
 
     header("Location: index.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
     die();
     }
 ?>
